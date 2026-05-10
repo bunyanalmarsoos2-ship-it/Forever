87f7d840534b0ddf0e0d6647fa1c3c926deeda47
Asghar <bunyanalmarsoos2@gmail.com>
2026-05-10 18:46:14 +0500

    Add BestSeller Seciton-1.35

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext.jsx";
 import Title from "./Title.jsx";
 import ProductItem from "./ProductItem.jsx";
+import BestSellers from "./BestSellers.jsx";
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
   const [latestProducts, setLatestProducts] = useState([]);
 
   useEffect(() => {
     setLatestProducts(products.slice(0, 10));
-  },[]);
+  }, []);
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
@@ -19,11 +20,16 @@ const LatestCollection = () => {
         </p>
       </div>
       {/* Rendering Products */}
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index)=>(
-         <ProductItem id={item._id} image = {item.image} name={item.name} price={item.price}/>
-    ))}
-    </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection.jsx'
+import BestSellers from '../components/BestSellers.jsx'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
+      <BestSellers/>
     </div>
   )
 }
