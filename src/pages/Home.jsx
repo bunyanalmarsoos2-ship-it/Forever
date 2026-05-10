b4adb633789a9a88d08cc9d081f119bd47676a27
Asghar <bunyanalmarsoos2@gmail.com>
2026-05-10 18:57:26 +0500

    Add Our Policy Section- 1.39

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection.jsx'
 import BestSellers from '../components/BestSellers.jsx'
+import OurPolicy from '../components/OurPolicy.jsx'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
       <BestSellers/>
+      <OurPolicy/>
     </div>
   )
 }
