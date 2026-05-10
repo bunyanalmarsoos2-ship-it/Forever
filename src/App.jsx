3dc31467393469e70771b8ba9be1d6d79c1b57a8
Asghar <bunyanalmarsoos2@gmail.com>
2026-05-10 19:34:15 +0500

    Add Footer Section - 1.50

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,13 @@ import Login from "./pages/Login";
 import Orders from "./pages/Orders";
 import PlaceOrder from "./pages/PlaceOrder";
 import Product from "./pages/Product";
-import Contact from './pages/Contact';
+import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
